3f2920ccaf2c288d0b8cf37bb162d3d7a9ec9c1f
Irene <airine@Irenes-MacBook-Air.local>
2020-08-28 12:20:59 -0700

    [3 components] Add three more components

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import {
 import Home from  './Home';
 import Procedures from './Procedures';
 import Contact from './Contact';
+import WidgetShow from './WidgetShow';
+import Career from './Career';
+import Cosmetic from './Cosmetic';
+import Doctors from './Doctors';
 
 function App() {
   const procedures = [
@@ -26,11 +30,18 @@ function App() {
         <nav>
           <Link to="/">Go back to Home</Link> {' '}
           <Link to="/procedures">See our procedures</Link> {' '}
+          <Link to="/cosmetic">Cosmetic</Link> {' '}
           <Link to="/contact">Contact us</Link> {' '}
+          <Link to="/doctors">Our Doctors</Link> {' '}
+          <Link to="/career">Career</Link> {' '}
         </nav>
         <Route exact path="/" component={Home} />
         <Route path="/procedures" render={() => <Procedures procedures={procedures} />} />
+        <Route path="/cosmetic" component={Cosmetic} />
         <Route path="/contact" component={Contact} />
+        <Route path="/doctors" component={Doctors} />
+        <Route path="/career" component={Career} />
+        <Route path="/widgets/:id" render={(props) => <WidgetShow procedures={procedures}{...props}/> } />
       </div>
     </Router>
     
---
+++ src/Cosmetic.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Cosmetic extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Cosmetic</h1>
+            </div>
+        )
+    }
+}
+
+export default Cosmetic
